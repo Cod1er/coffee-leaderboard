ad5fc2d07968676acee4cf1e9aa30cfb32516d93
João Coelho <JCoelhoo@users.noreply.github.com>
2018-04-17 14:38:52 +0100

    Update face_function.py

--- face_function.py
+++ face_function.py
@@ -1,31 +1,57 @@
 import boto3
-import json
 import time
 
-rekognition = boto3.client('rekognition')
+s3 = boto3.client('s3')
 dynamodb = boto3.client('dynamodb')
-s3_client = boto3.client('s3')
+rekognition = boto3.client('rekognition')
 
-FACE_COLLECTION = "Faces"
-FACE_MATCH_THRESHOLD = 70
+
+face_collection = "Faces" # Name of the face collection that the AWS Rekognition uses
+face_match_treshold = 70 # Match Treshold for the faces to be concidered the same person
+logging_table = 'logs' # DynamoDB table name for the log files
 
 
 
 def lambda_handler(event, context):
-    bucket = event['mybucket']
-    key = event['mykey']
 
-    img = {
+    utime = str(int(time.time())) #Current Unix Time   
+    coffee_cup_detected = False  
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']  
+    image = {
         'S3Object': {
             'Bucket': bucket,
-            'Name': key, }
-    }
+            'Name': key,
+            }
+        }
+    response = rekognition.detect_labels(Image=image, MaxLabels=123, MinConfidence=50)    
+    for object in response["Labels"]:
+        if object["Name"] == "Coffee Cup" or object["Name"] == "Cup":
+            coffee_cup_detected = True
+            break        
+    if coffee_cup_detected:
+        dynamodb.put_item(
+            TableName=logging_table,
+            Item={
+                'unixtime':{'S':utime},
+                'mymess':{'S': "Coffee Cup Detected"}
+            })
+        message = detect_faces(image, bucket, key)
+    else:
+        dynamodb.put_item(
+            TableName=logging_table,
+            Item={
+                'unixtime':{'S':utime},
+                'mymess':{'S': "Coffee Cup Not Found in the image"}
+                })
+        return 'Coffee Cup Not Found in the image'        
+    return message
 
-    time_now = str(int(time.time()))
+def detect_faces(image, bucket, key):
 
-    # Checks if user is already registered
-    faces = rekognition.search_faces_by_image(CollectionId=FACE_COLLECTION, Image=img,
-                                              FaceMatchThreshold=FACE_MATCH_THRESHOLD, MaxFaces=1)
+    # Checks if user face is already registered in rekongtion collection
+    faces = rekognition.search_faces_by_image(CollectionId=face_collection, Image=image,
+                                              FaceMatchThreshold=face_match_treshold, MaxFaces=1)
 
     if len(faces['FaceMatches']) == 1:  # User is already registered in the collection
         # Authenticate
@@ -33,74 +59,66 @@ def lambda_handler(event, context):
         item = dynamodb.get_item(TableName='faces', Key={'faceID': {'S': str(faceid)}})
 
         # Gets the item
-        print(item)
         item = item['Item']
         face_id = item['faceID']['S']
         score = int(item['score']['S'])
         unixtime = int(item['unixtime']['S'])
-
-        inc = str(score + 1)
-        time_now = int(time.time())
-        time_10_minutes_ago = time_now - 300
-        if unixtime < time_10_minutes_ago:
+        new_score = str(score + 1)
+        utime = int(time.time())#Current Unix Time
+        time_5_minutes_ago = int(utime) - 300
+        
+        #Checks if 5 minutes passed since the last upload
+        if unixtime < time_5_minutes_ago:
             dynamodb.update_item(TableName='faces', Key={'faceID': {'S': str(face_id)}},
-                                 UpdateExpression="set score = :val, unixtime =:val2, pathToImage=:val3",
+                                 UpdateExpression="set score = :val, unixtime =:val2, s3Bucket = :val3, pathToImage = :val4",
                                  ExpressionAttributeValues={
-                                     ':val': {'S': inc},
-                                     ':val2': {'S': str(time_now)},
-                                     ':val3': {'S': key}
-                                 }
-                                 )
+                                     ':val': {'S': new_score},
+                                     ':val2': {'S': str(utime)},
+                                     ':val3': {'S': bucket},
+                                     ':val4': {'S': key}
+                                 })
             dynamodb.put_item(
-                TableName='logs',
+                TableName=logging_table,
                 Item={
-                    'unixtime': {'S': str(time_now)},
-                    'mymess': {'S': 'User Score increased to ' + inc}
+                    'unixtime': {'S': str(utime)},
+                    'mymess': {'S': 'User Score increased to ' + new_score}
                 })
-            return "User Score increased"
+            return 'User Score increased to ' + new_score
         else:
             dynamodb.put_item(
-                TableName='logs',
+                TableName=logging_table,
                 Item={
-                    'unixtime': {'S': time_now},
-                    'mymess': {'S': 'Sorry, you can only participate every 5 minutes.)'}
+                    'unixtime': {'S': str(utime)},
+                    'mymess': {'S': 'Sorry, you can only participate every 5 minutes.'}
                 })
-            return "You can only participate every 5 minutes, sorry mate"
+            return 'You can only participate every 5 minutes'
     else:
         # Face not found in the Rekognition database
-        faces = rekognition.index_faces(Image=img, CollectionId=FACE_COLLECTION)
+        faces = rekognition.index_faces(Image=image, CollectionId=face_collection)
 
         # Check if there are no faces in the image:
         if len(faces['FaceRecords']) == 0:
-
-            dynamodb.put_item(  # log
-                TableName='logs',
+           dynamodb.put_item( 
+                TableName=logging_table,
                 Item={
-                    'unixtime': {'S': time_now},
+                    'unixtime': {'S': str(utime)},
                     'mymess': {'S': "No faces were found in the picture"}
                 })
-
-            return json.dumps({
-                'success': False,
-                'message': 'No face found in the image'
-            })
+            return 'No faces found in the image'
 
         # More than one face in the image:
         elif len(faces['FaceRecords']) > 1:
-            rekognition.delete_faces(CollectionId=FACE_COLLECTION,
+            rekognition.delete_faces(CollectionId=face_collection,
                                      FaceIds=[f['Face']['FaceId'] for f in faces['FaceRecords']])
 
             dynamodb.put_item(
-                TableName='logs',
+                TableName=logging_table,
                 Item={
-                    'unixtime': {'S': time_now},
-                    'mymess': {'S': "Error: More than one face detected in the image... Destruction in progress"}
+                    'unixtime': {'S': str(utime)},
+                    'mymess': {'S': "Error: More than one face detected in the image"}
                 })
 
-            return json.dumps({
-                'success': False,
-                'message': 'More than one face in the image'
-            })
+            return 'More than one face in the image'
 
         # One new face in the image, register it:
         else:
@@ -110,15 +128,16 @@ def lambda_handler(event, context):
                 Item={
                     'faceID': {'S': face_id},
                     'score': {'S': '1'},
-                    'unixtime': {'S': str(int(time.time()))},
-                    'pathToImage': {'S': str(key)}
+                    'unixtime': {'S': str(utime)},
+                    's3Bucket': {'S': bucket},
+                    'pathToImage': {'S': key}
                 })
 
-            response = dynamodb.put_item(
-                TableName='logs',
+            dynamodb.put_item(
+                TableName=logging_table,
                 Item={
-                    'unixtime': {'S': time_now},
+                    'unixtime': {'S': str(utime)},
                     'mymess': {'S': "Congratulations! You're now registered for the coffee leaderboard."}
                 })
 
-            return response
+            return "Congratulations! You're now registered for the coffee leaderboard."
